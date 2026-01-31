30e0382bb806e942be30e7067a487326b058a3fd
GabrielChagas <gabrielserq@outlook.com>
2026-01-31 18:27:33 -0300

    studying double, float e decimal data types

---
+++ Fundamentos/TipoDeDados002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Atribuindo valores a double, float e decimal");
+Console.WriteLine();
+
+double n1 = 3.14;
+float n2 = 3.14f;
+decimal n3 = 3.14m;
+
+Console.WriteLine($"Valores => {n1} {n2} {n3}");
+
+Console.ReadLine();
+
+Console.WriteLine("Comparando a precisão de float, double e decimal");
+float x = 1f /3f;
+double y = 1d /3d;
+decimal z = 1m /3m;
+Console.WriteLine($"Valores => {x} {y} {z}");
+
+Console.ReadLine();
