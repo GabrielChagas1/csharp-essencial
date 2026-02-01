5535204409e4a97a62637c38346c96c210e0038d
GabrielChagas <gabrielserq@outlook.com>
2026-02-01 00:54:09 -0300

    Add data type demonstrations and project configuration
    Updated `Program.cs` to include examples of boolean, character, and integer assignments and comparisons. Added user input prompt at the end.
    
    Modified `TiposDeDados003.csproj` to set project output type to executable, target .NET 8.0, and enable implicit usings and nullable reference types.

---
+++ Fundamentos/TiposDeDados003/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Atribuindo valores para bool e char");
+
+bool ativo = true;
+Boolean ativo2 = false;
+
+Console.WriteLine($"{ativo}, {ativo2}");
+
+Console.WriteLine(10 == 15);
+Console.WriteLine(10 == 10);
+
+int x = 10;
+int y = 20;
+
+Console.WriteLine(x > y);
+
+char letra = 'A';
+char letra1 = '\u0041';
+char letra2 = (char)90;
+
+Console.WriteLine($"{letra}, {letra1}, {letra2}");
+
+
+Console.ReadLine();
