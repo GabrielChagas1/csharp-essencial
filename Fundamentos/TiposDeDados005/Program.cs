ed0036e67c32f2d3a0fa504b99ee0a467ab1d41d
GabrielChagas <gabrielserq@outlook.com>
2026-02-01 00:54:40 -0300

    Add date and time display functionality
    Updated `Program.cs` to display current date and time,
    manipulate date values, and format output.
    Modified `TiposDeDados005.csproj` to set project settings
    for .NET SDK, output type, target framework, and nullable
    reference types.

---
+++ Fundamentos/TiposDeDados005/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Data e hora struct");
+
+DateTime now = DateTime.Now;
+
+Console.WriteLine(now.Year);
+Console.WriteLine(now.Month);
+Console.WriteLine(now.Day);
+Console.WriteLine(now.Hour);
+Console.WriteLine(now.Second);
+Console.WriteLine(now.Millisecond);
+
+//adicionando valores
+Console.WriteLine(now.AddDays(30));
+Console.WriteLine(now.AddMonths(1));
+Console.WriteLine(now.AddHours(2));
+Console.WriteLine(now.AddYears(5));
+
+//obter o dia da semana e do ano
+Console.WriteLine(now.DayOfWeek);
+Console.WriteLine(now.DayOfYear);
+
+//data no formato curto e longo
+Console.WriteLine(now.ToShortDateString());
+Console.WriteLine(now.ToLongDateString());
+
+
+
+Console.ReadLine();
