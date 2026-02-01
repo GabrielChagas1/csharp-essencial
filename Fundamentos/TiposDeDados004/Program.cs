46eba19ce183fb1a832128eee6843aef425ac617
GabrielChagas <gabrielserq@outlook.com>
2026-02-01 00:54:23 -0300

    Add data type demonstrations and project settings
    Updated `Program.cs` to include examples of string, object,
    and dynamic type assignments with console output. Modified
    `TiposDeDados004.csproj` to set project properties such as
    output type and target framework.

---
+++ Fundamentos/TiposDeDados004/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Atribuindo valores string, object e dynamic");
+Console.WriteLine();
+
+string nome = "João da Silva";
+String nome2 = "Maria Oliveira";
+
+Console.WriteLine(nome, nome2);
+
+string valor = "Isto é uma string";
+valor = "Isto é uma string alterada";
+valor = "teste";
+
+object nota = 10;
+object valor1 = 8.55m;
+object ativa = true;
+
+dynamic variavel = 10;
+
+Console.WriteLine(valor);
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(ativa);
+Console.WriteLine(variavel);
+
+
+Console.ReadLine();
