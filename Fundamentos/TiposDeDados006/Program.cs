e9f00e057e3b637c2c05b60a4972fe14ac59f594
GabrielChagas <gabrielserq@outlook.com>
2026-02-02 14:16:22 -0300

    Add nullable types demonstration and project configuration
    Updated `Program.cs` to showcase nullable types in C# with examples and conditional checks. Modified `TiposDeDados006.csproj` to target .NET 8.0, enable implicit usings, and support nullable reference types.

---
+++ Fundamentos/TiposDeDados006/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Nullable Types");
+
+Nullable<int> i = null;
+Nullable<double> d = null;
+Nullable<bool> c = null;
+
+Console.WriteLine(i);
+Console.WriteLine(d);
+Console.WriteLine(c);
+
+
+//simplicação
+int? teste = null;
+Console.WriteLine(teste);
+
+int? a = null;
+int b = a ?? 10;
+Console.WriteLine(b);
+
+int? value = null;
+if(value.HasValue)
+{
+    Console.WriteLine($"O valor é {value.Value}");
+}
+else
+{
+    Console.WriteLine("O valor é nulo");
+}
+
+Console.ReadLine();
