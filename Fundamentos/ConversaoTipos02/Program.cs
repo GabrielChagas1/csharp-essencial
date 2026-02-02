d646524bd501cde90e29cf11932674c036d007a2
GabrielChagas <gabrielserq@outlook.com>
2026-02-02 14:17:10 -0300

    Create project and implement type conversion examples
    Added `ConversaoTipos02.csproj` with .NET 8.0 SDK format, enabling implicit usings and nullable reference types. Updated `Program.cs` to demonstrate type conversions for integers, doubles, decimals, and booleans, including console output for each conversion.

---
+++ Fundamentos/ConversaoTipos02/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("## Conversão de tipos ##\n");
+
+int valorInt = 123;
+double valorDouble = 12.45;
+decimal valorDecimal = 567.89M;
+
+string str1 = valorInt.ToString();
+string str2 = valorDouble.ToString();
+string str3 = valorDecimal.ToString();
+Console.WriteLine($"{str1}, {str2}, {str3}");
+
+int valorInt2 = 10;
+double valorDouble2 = 3.14;
+bool valorBool = true;
+
+Console.WriteLine(Convert.ToString(valorInt2));
+Console.WriteLine(Convert.ToDouble(valorInt2));
+Console.WriteLine(Convert.ToString(valorBool));
+Console.WriteLine(Convert.ToInt32(valorDouble2));
+
+int varInt3 = 10000;
+Console.WriteLine(Convert.ToByte(varInt3));
+
+Console.ReadLine();
