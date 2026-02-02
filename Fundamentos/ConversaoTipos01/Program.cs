151515dd0d12ecce26092b1b015ccbb490cb8338
GabrielChagas <gabrielserq@outlook.com>
2026-02-02 14:17:01 -0300

    Add .NET 8.0 project and type conversion examples
    Created `ConversaoTipos01.csproj` with .NET SDK format targeting framework 8.0.
    Updated `Program.cs` to include type conversion examples, demonstrating conversions between int, double, long, float, and decimal, along with a division calculation.
    Added `Console.ReadLine()` to pause output.

---
+++ Fundamentos/ConversaoTipos01/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("## Conversão de tipos ##\n");
+
+int varInt = 100;
+double varDouble = varInt;
+Console.WriteLine(varDouble);
+
+int numeroInt = 2145678;
+long numeroLong = numeroInt;
+float numeroFloat = numeroInt;
+double numeroDouble = numeroInt;
+decimal numeroDecimal = numeroInt;
+Console.WriteLine($"{numeroInt}, {numeroLong}, {numeroFloat}, {numeroDouble}, {numeroDecimal}");
+
+
+double varDouble2 = 1234.56;
+int varInt2 = (int)varDouble2;
+Console.WriteLine(varInt2);
+
+int num1 = 10;
+int num2 = 4;
+
+float resultadoFloat = (float)num1 / (float)num2;
+Console.WriteLine(resultadoFloat);
+
+
+Console.ReadLine();
