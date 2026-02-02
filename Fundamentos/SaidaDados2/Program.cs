1021c60827ea004493139883d78fde66195b7c03
GabrielChagas <gabrielserq@outlook.com>
2026-02-02 14:16:48 -0300

    Add escape sequence examples and update project settings
    Updated `Program.cs` to demonstrate escape sequences with new string variables and added a console read line for user interaction. Modified `SaidaDados2.csproj` to set project SDK, output type, target framework, and enable implicit usings and nullable reference types.

---
+++ Fundamentos/SaidaDados2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Saida de dados: Usando sequencia de escapes");
+
+string local = "c:\\dados\\arquivos";
+
+string frase = "Ele disse: \"Não fui eu\"";
+
+string pizza = "\nPizza\nde\nCalabresa";
+
+string bolo = "\nBolo\tde\tChocolate";
+
+Console.WriteLine($"{local} {frase} {bolo} {pizza}");
+
+
+
+Console.ReadLine();
