9c6a397643cc0095b88b5b538a5ca3250184b2d0
GabrielChagas <gabrielserq@outlook.com>
2026-02-03 10:56:31 -0300

    Reapply "Update projects to .NET 8.0 and enhance functionality"
    This reverts commit e828f8899ba9fb446d7b03773e6ea60af6a09435.

---
+++ Fundamentos/PrecendeciaOperadores/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## Precedência de operadores ##\n");
+
+//int x = 10 - 2 * 3;
+//int y = (10 - 2) * 3;
+
+//Console.WriteLine(x);
+//Console.WriteLine(y);
+
+//bool b = !(9 != 8) && 5 >= 7 || 8>= 6;
+//Console.WriteLine(b);
+
+//bool c = !(9 != 8) && (5 >= 7 || 8 >= 6);
+//Console.WriteLine(c);
+
+int a = 5, b = 6, c = 4;
+int r = --a * b - ++c;
+Console.WriteLine(r);
+
+Console.ReadKey();
