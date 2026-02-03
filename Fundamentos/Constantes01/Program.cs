cba4ed4e07424425ececdd1d7d13f4936559d0aa
GabrielChagas <gabrielserq@outlook.com>
2026-02-03 11:12:32 -0300

    Set up .NET 8.0 project and implement circle calculations
    - Updated `Constantes01.csproj` to use .NET SDK, set output type to executable, and target .NET 8.0 with implicit usings and nullable reference types enabled.
    - Modified `Program.cs` to add a header output, comment out unused constants, and introduce variables for radius, perimeter, and area.
    - Implemented user input for radius, calculated perimeter and area of a circle, and added a pause before closing the console.

---
+++ Fundamentos/Constantes01/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("## Constantes ##\n");
+
+//const int ANO = 2024;
+//const int MES = 12, SEMANA = 13, QUINZENA = 2;
+
+//const int MESES_ANO = 12;
+//const int DIAS_ANO = 365;
+
+//const float DIAS_POR_MES = (float)DIAS_ANO / MESES_ANO;
+//Console.WriteLine(DIAS_POR_MES);
+
+double raio, perimetro, area;
+//const double PI = 3.14159;
+
+Console.Write("Digite o valor do raio: ");
+raio = Convert.ToDouble(Console.ReadLine());
+
+perimetro = 2 * Math.PI * raio;
+area = Math.PI * Math.Pow(raio, 2);
+
+Console.WriteLine($"Perimetro: {perimetro}, Raio: {area}");
+
+
+
+
+Console.ReadKey();
+
