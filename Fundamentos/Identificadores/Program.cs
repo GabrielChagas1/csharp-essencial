0f47f6e05ba644dfea2801b8acb3eb8654438122
GabrielChagas <gabrielserq@outlook.com>
2026-02-04 09:02:17 -0300

    chore: add identificadores

---
+++ Fundamentos/Identificadores/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Definindo Identificadores");
+Console.WriteLine();
+
+// Identificadores válidos
+string nomeCompleto = "Ana Paula";
+string nome = "Carlos";
+int idade = 28;
+int _valor = 100;
+int idade1 = 0;
+
+// Identificadores inválidos
+//int 5idade;
+//int $valor;
+//int valor#total;
+//string nome completo;
+
+//camel case
+string descontoTotal;
+string desconto_total;
+
+//constantes em maiúsculo
+double PI = 3.1415;
+double TAXA_JUROS = 0.05;
+string PAIS = "Brasil";
+
+Console.ReadLine();
+
+// Pascal case
+class ImprimirTexto
+{
+    public void Imprimir()
+    {
+        Console.WriteLine("Imprimindo texto...");
+    }
+}
+
+
