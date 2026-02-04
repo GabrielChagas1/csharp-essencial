898e4c4ba2af51e919f9b3e6c82bf246e0bf8f15
GabrielChagas <gabrielserq@outlook.com>
2026-02-04 09:02:07 -0300

    chore: add entradadedados01

---
+++ Fundamentos/EntradaDeDados01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("\n ## Entrada de dados ##");
+
+Console.WriteLine("\nInforme seu nome");
+string nome = Console.ReadLine();
+
+Console.WriteLine("\nInforme sua idade");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"O seu nome é {nome} e sua idade é {idade}");
+
+Console.ReadKey();
