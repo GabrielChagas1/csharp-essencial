0235c16c532fa8e817e80d15882d193cff71e1c2
GabrielChagas <gabrielserq@outlook.com>
2026-02-08 17:09:50 -0300

    chore: finished fundamentos

---
+++ EstruturasControle/EstruturasControle/001-EstruturasIF/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("## Estruturas IF ##");
+
+//Console.Write("Cliente especial (S/N): ");
+//string clienteEspecial = Console.ReadLine();
+
+//if(clienteEspecial == "S" || clienteEspecial == "s")
+//{
+//    Console.WriteLine("Desconto de 10% aplicado!");
+//}
+
+Console.Write("Informe o valor de x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Informe o valor de y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > y)
+{
+    Console.WriteLine("X é maior que Y");
+}
+if(x < y)
+{
+    Console.WriteLine("Y é maior que X");
+}
+if(x == y)
+{
+    Console.WriteLine("X é igual a Y");
+}
+
+
+Console.ReadKey();
