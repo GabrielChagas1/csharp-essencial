56220338606a5d7689e84e545dd1ab5f8ff95593
GabrielChagas <gabrielserq@outlook.com>
2026-02-09 21:05:52 -0300

    chore: add while

---
+++ EstruturasControle/EstruturasControle/007-while/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("## Estrutura de repetição while ##\n");
+
+//var i = 0;
+
+//while (i <= 10)
+//{
+//    Console.WriteLine($"i = {i}");
+//    i++;
+//}
+
+//var x = 10;
+
+//while (x >= 0)
+//{
+//    Console.WriteLine($"x = {x}");
+//    x--;
+//}
+
+Console.Write("Digite um número: ");
+var numero = Convert.ToInt32(Console.ReadLine());
+var contador = 0;
+
+if (numero < 0)
+{
+    Console.WriteLine("Número inválido! Digite um número maior ou igual a zero.");
+}
+else
+{
+    while (contador <= numero)
+    {
+        Console.WriteLine($"Contador = {contador}");
+        contador++;
+    }
+}
+
+
+Console.ReadKey();
