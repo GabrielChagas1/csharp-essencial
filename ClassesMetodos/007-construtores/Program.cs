e42828d6b76b2f180f2d93aa7800ab26e5a3b679
GabrielChagas <gabrielserq@outlook.com>
2026-03-10 10:52:57 -0300

    chore: add 007-construtores

---
+++ ClassesMetodos/007-construtores/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("## Construtores ##");
+
+Aluno aluno = new Aluno("Gabriel", 26, "masculino", "S");
+
+
+Console.ReadKey();
+
+
+public class Aluno
+{
+    public string? Nome;
+    public int Idade;
+    public string Sexo;
+    public string Aprovado;
+    public Aluno(string nome, int idade, string sexo, string aprovado)
+    {
+        Nome = nome;
+        Idade = idade;
+        Sexo = sexo;
+        Aprovado = aprovado;
+    }
+}
