b30ed7d198fef078c6cda99db5c439ba7ced9f5f
GabrielChagas <gabrielserq@outlook.com>
2026-03-10 10:52:20 -0300

    chore: add 0004-metodos

---
+++ ClassesMetodos/004-metodos/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("## Métodos ##");
+
+
+MinhaClasse minhaClasse = new MinhaClasse();
+minhaClasse.MeuMetodo();
+minhaClasse.Saudacao("Gabriel");
+minhaClasse.ExibirDataHora();
+
+
+Console.ReadKey();
+
+
+class MinhaClasse
+{
+    public void MeuMetodo()
+    {
+        Console.WriteLine("Este é um método da minha classe.");
+    }
+
+    public void Saudacao(string nome)
+    {
+        Console.WriteLine($"Olá, {nome}!");
+    }
+
+    public void ExibirDataHora()
+    {
+        Console.WriteLine($"Data e hora atuais: {DateTime.Now.ToShortDateString()}");
+    }
+}
