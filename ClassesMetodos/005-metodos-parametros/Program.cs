993d7df684df070be23ec44f5602cdf2592b1214
GabrielChagas <gabrielserq@outlook.com>
2026-03-10 10:52:37 -0300

    chore: add 005-metodos-parametros

---
+++ ClassesMetodos/005-metodos-parametros/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("## Métodos ##");
+
+
+MinhaClasse minhaClasse = new MinhaClasse();
+minhaClasse.Saudacao("Gabriel", DateTime.Now);
+
+Console.ReadKey();
+
+
+class MinhaClasse
+{
+    public void Saudacao(string nome, DateTime dataHora)
+    {
+        Console.WriteLine($"Olá, {nome}! Hoje é {dataHora.ToShortDateString()}");
+    }
+}
