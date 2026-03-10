5397d1df92744be685661182711849b3cd0e5aaf
GabrielChagas <gabrielserq@outlook.com>
2026-03-10 10:52:43 -0300

    chore: add 006-metodos-parametros

---
+++ ClassesMetodos/006-metodos-parametros/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("## Métodos ##");
+
+Aluno aluno1 = new Aluno
+{
+    Nome = "Gabriel",
+    Idade = 25,
+    Sexo = "Masculino",
+    Aprovado = "S"
+};
+
+Curso curso = new();
+curso.Resultado(aluno1);
+
+
+Console.ReadKey();
+
+public class Aluno
+{
+    public string? Nome;
+    public int Idade;
+    public string Sexo;
+    public string Aprovado;
+}
+
+public class Curso
+{
+    public void Resultado(Aluno aluno)
+    {
+        Console.WriteLine($"O aluno {aluno.Nome}, sexo {aluno.Sexo} com idade {aluno.Idade} anos");
+        if(aluno.Aprovado == "S")
+        {
+            Console.WriteLine("foi aprovado!");
+        }
+        else
+        {
+            Console.WriteLine("foi reprovado!");
+        }
+    }
+}
