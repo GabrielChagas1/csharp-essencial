b5c4bba57223b0102765751301d3b9de71571ee1
GabrielChagas <gabrielserq@outlook.com>
2026-03-11 16:39:30 -0300

    chore: add 012-sobrecarga-metodos

---
+++ ClassesMetodos/012-sobrecarga-metodos/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("## Métodos com retorno ##");
+
+Email mail = new();
+
+
+Console.ReadKey();
+
+public class Email
+{
+    public string EnviarEmail(string destinatario, string assunto, string mensagem)
+    {
+        // Lógica para enviar o email
+        return $"Email enviado para {destinatario} com assunto '{assunto}'";
+    }
+    public string EnviarEmail(string destinatario, string assunto)
+    {
+        // Lógica para enviar o email sem mensagem
+        return $"Email enviado para {destinatario} com assunto '{assunto}' e sem mensagem";
+    }
+
+    public string EnviarEmail(decimal assunto, string destinatario)
+    {
+        // Lógica para enviar o email sem mensagem
+        return $"Email enviado para {destinatario} com assunto '{assunto}' e sem mensagem";
+    }
+}
