d95b37a2bdc1dce7f321b2eeeedc5a77439710f6
GabrielChagas <gabrielserq@outlook.com>
2026-03-11 16:39:01 -0300

    chore: add 009-exercicios

---
+++ ClassesMetodos/009-exercicios/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("## Exercicios 01 ##");
+
+Carro chevrolet = new
+(
+    modelo: "Sedan",
+    montadora: "Chevrolet",
+    marca: "Onix",
+    ano: 2020,
+    potencia: 100
+);
+chevrolet.Exibir();
+chevrolet.Acelerar();
+
+Carro ford = new
+(
+    modelo: "Hatch",
+    montadora: "Ford",
+    marca: "Ka",
+    ano: 2021,
+    potencia: 120
+);
+
+ford.Exibir();
+ford.Acelerar();
+
+
+public class Carro
+{
+    public string modelo;
+    public string montadora;
+    public string marca;
+    public int ano;
+    public int potencia;
+
+    public Carro(string modelo, string montadora, string marca, int ano, int potencia)
+    {
+        this.modelo = modelo;
+        this.montadora = montadora;
+        this.marca = marca;
+        this.ano = ano;
+        this.potencia = potencia;
+    }
+
+    public void Exibir()
+    {
+        Console.WriteLine($"O meu carro é do modelo {modelo}, da montadora {montadora} da marca {marca}, do ano {ano} e potência de {potencia}");
+    }
+
+
+    public void Acelerar()
+    {
+        Console.WriteLine($"Acelerando o meu {this.marca} \n");
+    }
+
+}
