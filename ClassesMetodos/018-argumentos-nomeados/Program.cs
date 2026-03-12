a13bacd520605d3150c014318325d9c9362707b7
GabrielChagas <gabrielserq@outlook.com>
2026-03-12 09:57:54 -0300

    chore: add 018-argumentos-nomeados

---
+++ ClassesMetodos/018-argumentos-nomeados/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("## Argumentos Nomeados ##");
+
+Email email = new Email();
+email.EnviarEmail("teste@gmail.com", "Urgente", "Olá, este é um email de teste.");
+email.EnviarEmail(destinatario: "teste@gmail.com", mensagem: "Olá, este é um email de teste.", assunto: "Urgente");
+
+Console.ReadKey();
+
+
+public class Email
+{
+    public void EnviarEmail(string destinatario, string assunto, string mensagem)
+    {
+        Console.WriteLine($"Enviando email para: {destinatario}");
+        Console.WriteLine($"Assunto: {assunto}");
+        Console.WriteLine($"Mensagem: {mensagem} \n");
+    }
+}
