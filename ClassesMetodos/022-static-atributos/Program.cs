d4d35498f5f0ace36208099bf678453e3ddbd97e
GabrielChagas <gabrielserq@outlook.com>
2026-03-13 11:39:21 -0300

    chore: 020-static-atributos

---
+++ ClassesMetodos/022-static-atributos/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("## Atributos estáticos ##");
+
+A a1 = new();
+a1.x = 10;
+
+A a2 = new();
+a2.x = 20;
+
+A.y = 30;
+
+Console.WriteLine($"a1.x = {a1.x} a2.x = {a2.x} A.y = {A.y}");
+
+Console.ReadKey();
+
+public class A
+{
+    public int x;
+    public static int y;
+
+}
+
