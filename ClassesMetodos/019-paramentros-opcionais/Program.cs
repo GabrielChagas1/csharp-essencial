d82d46e5c10b48e8a44c0acc69235f05dbbd2646
GabrielChagas <gabrielserq@outlook.com>
2026-03-13 11:38:53 -0300

    chore: add 019-parametros opcionais

---
+++ ClassesMetodos/019-paramentros-opcionais/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("## Paramentros opcionais ##");
+
+Email email = new Email();
+email.EnviarEmail("teste@gmail.com");
+email.EnviarEmail(destinatario: "teste@gmail.com", mensagem: "Olá, este é um email de teste.", assunto: "Urgente");
+
+Console.ReadKey();
+
+public class Email
+{
+    public void EnviarEmail(string destinatario, string assunto = "Assunto Padrão", string mensagem = "Mensagem Padrão")
+    {
+        Console.WriteLine($"Enviando email para: {destinatario}");
+        Console.WriteLine($"Assunto: {assunto}");
+        Console.WriteLine($"Mensagem: {mensagem} \n");
+    }
+}
