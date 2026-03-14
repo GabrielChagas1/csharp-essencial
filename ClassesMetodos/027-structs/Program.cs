11ffe6ca8e9b82eb34c206e9d7a06436d0b9868e
GabrielChagas <gabrielserq@outlook.com>
2026-03-14 16:25:23 -0300

    chore: add 027-structs

---
+++ ClassesMetodos/027-structs/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Structs");
+
+//Cliente cliente = new()
+//{
+//    Nome = "Maria",
+//    Idade = 30
+//};
+
+MinhaClasse c1 = new() { x = 10, y = 20 };
+MinhaStruct s1 = new() { x = 10, y = 20 };
+
+Console.ReadKey();
+
+
+public class MinhaClasse 
+{
+    public int x;
+    public int y;
+}
+
+struct MinhaStruct
+{
+    public int x;
+    public int y;
+}
+
+
+struct Cliente
+{
+    public string? Nome;
+    public int Idade { get; set; }
+
+    public Cliente(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+}
