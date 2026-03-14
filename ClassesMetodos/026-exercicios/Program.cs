9ab1ef1ca08069d424a705106a034a389394cf63
GabrielChagas <gabrielserq@outlook.com>
2026-03-14 16:25:13 -0300

    chore: add 026-exercicios

---
+++ ClassesMetodos/026-exercicios/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("## Exercícios estáticos ##");
+
+Carro carro1 = new()
+{
+    Modelo = "Civic",
+    Marca = "Honda",
+    Ano = 2025
+};
+
+Carro.ObterValorIPVA();
+Console.WriteLine($"Valor do IPVA: {Carro.valorIPVA}");
+Console.WriteLine($"Ano: {carro1.Ano}");
+
+Console.ReadKey();
+
+public class Carro
+{
+    public string? Modelo;
+    public string? Marca;
+    public static double valorIPVA; // 4% do valor do carro
+
+    private int ano;
+    public int Ano
+    {
+        get { return ano; }
+        set
+        {
+            if (value < 2000)
+                ano = 2000;
+            else if (value > 2022)
+                ano = 2022;
+        }
+    }
+
+    public static void ObterValorIPVA()
+    {
+        valorIPVA = 0.04;
+    }
+
+}
