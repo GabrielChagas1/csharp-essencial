a6d7f07add4afc941776ecdfeba38dcf402a89bf
GabrielChagas <gabrielserq@outlook.com>
2026-03-14 16:24:51 -0300

    chore: add 024-static-constants

---
+++ ClassesMetodos/024-static-construtores/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿public class Pessoa
+{
+    public string? Nome;
+    public int Idade;
+    public static int IdadeMinima;
+    static Pessoa()
+    {
+        IdadeMinima = 18;
+        Console.WriteLine("Construtor estático chamado.");
+    }
+    public Pessoa()
+    {
+        Console.WriteLine("Construtor de instância padrão chamado.");
+    }
+    public Pessoa(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+        Console.WriteLine("Construtor de instância chamado.");
+    }
+}
---
+++ ClassesMetodos/024-static-construtores/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("## Construtor estáticos ##");
+
+Pessoa p1 = new("João", 25);
+Console.WriteLine($"Pessoa: {p1.Nome}, Idade: {p1.Idade}, Idade Mínima: {Pessoa.IdadeMinima}");
+
+Console.ReadKey();
