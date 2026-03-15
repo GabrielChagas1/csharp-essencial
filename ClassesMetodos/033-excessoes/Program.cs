ac7a1e76ceea1185f358c1071914aa49bc709f70
GabrielChagas <gabrielserq@outlook.com>
2026-03-15 16:51:47 -0300

    chore: add 0233-excessoes

---
+++ ClassesMetodos/033-excessoes/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Execessões");
+
+
+try
+{
+    Console.Write("Informe o valor de x: ");
+    int x = int.Parse(Console.ReadLine());
+
+    Console.Write("Informe o valor de y: ");
+    int y = int.Parse(Console.ReadLine());
+
+    int z = x / y;
+
+    Console.WriteLine($"O resultado de {x} / {y} é: {z}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex?.StackTrace?.ToString());
+
+	Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Fim do programa");
+}
+
+Console.ReadKey();
