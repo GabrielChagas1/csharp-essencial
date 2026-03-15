deaff8d11be752eb6076d0ce741b2486ff86f97c
GabrielChagas <gabrielserq@outlook.com>
2026-03-15 16:47:36 -0300

    chore: add 029-enum

---
+++ ClassesMetodos/029-enum/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enum");
+
+Console.WriteLine(DiasDaSemana.Domingo);
+
+int dia1 = (int)DiasDaSemana.Domingo;
+Console.WriteLine(dia1);
+
+Console.WriteLine((Categorias)1);
+
+Console.ReadKey();
+
+enum Categorias
+{
+    Moda, 
+    Automoveis,
+    Artes,
+    Esportes
+}
+
+
+enum DiasDaSemana
+{
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado,
+    Domingo
+}
