156dfad471c04e3250b66a9506fa6eb9a2c40582
GabrielChagas <gabrielserq@outlook.com>
2026-03-17 10:16:00 -0300

    chore: add 034-tipos-anonimos

---
+++ ClassesMetodos/034-tipos-anonimos/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Tipos Anonimos");
+
+var pessoa = new { Nome = "João", Idade = 30 };
+Console.WriteLine($"{pessoa.Nome} - {pessoa.Idade}");
+Console.WriteLine(pessoa.GetType());
+
+Console.ReadKey();
