54f52140d4d1fc07a1bc5da2c6dd474434465a52
GabrielChagas <gabrielserq@outlook.com>
2026-03-17 10:17:00 -0300

    chore: add 004-modificador params

---
+++ EstruturasRepeticao/004-modificador-params/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## Modificador Params ##");
+
+Console.WriteLine("\nSoma de números inteiros\n");
+
+Calculadora calculadora = new ();
+int resultadoSoma = calculadora.Somar(1, 2, 3, 4, 5);
+Console.WriteLine($"O resultado da soma é: {resultadoSoma}");
+
+Console.ReadKey();
+
+public class Calculadora
+{
+    public int Somar(params int[] numeros)
+    {
+        //int resultado = 0;
+        //foreach (int numero in numeros)
+        //{
+        //    resultado += numero;
+        //}
+        //return resultado;
+        return numeros.Sum();
+    }
+}
