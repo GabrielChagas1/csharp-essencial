eb9c5de667064e6758d1a5803554d347dbba5a93
GabrielChagas <gabrielserq@outlook.com>
2026-03-18 08:58:41 -0300

    chore: add 006-exercicios

---
+++ EstruturasRepeticao/006-exercicios/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## Exercicios Array Multidimensionais ##");
+
+string[,] alunos = new string[2, 5];
+
+for (int i = 0; i < alunos.GetLength(0); i++)
+{
+	for (int j = 0; j < alunos.GetLength(1); j++)
+	{
+		Console.Write($"Digite o nome do aluno na linha {i} e coluna {j}: ");
+		alunos[i, j] = Console.ReadLine() ?? "";
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < alunos.GetLength(0); i++)
+{
+    for (int j = 0; j < alunos.GetLength(1); j++)
+    {
+        Console.Write($"{alunos[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
