e057471f532c540c63a2ebfba1e5b0b6441a05f6
GabrielChagas <gabrielserq@outlook.com>
2026-03-18 08:59:04 -0300

    chore: add 008-arraylist-metodos

---
+++ EstruturasRepeticao/008-arraylist-metodos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+Console.WriteLine("## ArrayList ##");
+
+ArrayList list2 = [5, 8, 1, 3, 2];
+
+
+ArrayList list =
+[
+    "Maria",
+    5,
+    true,
+    " ",
+    null,
+];
+
+list.Add(3.5);
+list.Insert(2, "Paulo");
+
+int[] array = [1, 2, 3];
+
+list.AddRange(array);
+list.InsertRange(1, array);
+
+//remove apenas a primeira ocorrência do item
+list.Remove(null);
+
+//remove o item do índice especificado
+list.RemoveAt(0);
+
+//remove uma quantidade de itens a partir do índice especificado
+list.RemoveRange(0, 2);
+
+//verificar se o item existe na lista
+bool isConstains = list.Contains("Paulo");
+Console.WriteLine(isConstains);
+
+//Ordena a lista, mas só funciona se os itens forem do mesmo tipo e comparáveis
+list2.Sort();
+
+//Remove todos os itens da lista
+list.Clear();
+
+
+Console.ReadKey();
