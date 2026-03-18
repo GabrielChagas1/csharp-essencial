3aece73635d90358ebf9131076139c3b9e01774b
GabrielChagas <gabrielserq@outlook.com>
2026-03-18 08:58:54 -0300

    chore: add 007-arraylist

---
+++ EstruturasRepeticao/007-arraylist/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+Console.WriteLine("## ArrayList ##");
+
+ArrayList list = [];
+
+list.Add("Hello");
+list.Add("World");
+
+ArrayList list2 = [5];
+list2.Add("RWARW");
+list2.Capacity.ToString();
+
+ArrayList list3 = [5, "RWARW", 3.14];
+
+foreach (var item in list)
+{
+    Console.Write($"{item} ");
+}
+
+Console.WriteLine();
+
+
+foreach (var item in list2)
+{
+    Console.Write($"{item} ");
+}
+
+Console.WriteLine();
+
+
+foreach (var item in list3)
+{
+    Console.Write($"{item} ");
+}
+
+Console.WriteLine();
+
+Console.ReadKey();
