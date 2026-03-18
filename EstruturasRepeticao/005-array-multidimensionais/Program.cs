e2b4d6b8f1b7e36c774c8b6abe68772d00ffed49
GabrielChagas <gabrielserq@outlook.com>
2026-03-18 08:58:28 -0300

    chore: add 005-array-multidimensionais

---
+++ EstruturasRepeticao/005-array-multidimensionais/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## Array Multidimensionais ###");
+
+int[,] a = new int[2, 3]
+{
+    {1, 2, 3},
+    {4, 5, 6}
+};
+
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    for (int j = 0; j < a.GetLength(1); j++)
+    {
+        Console.Write($"{a[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+foreach (int valor in a)
+{
+    Console.Write($"{valor} ");
+}
+
+Console.ReadKey();
