4fe73d0a10d59c6f38a618575c46ffcf3cc35907
GabrielChagas <gabrielserq@outlook.com>
2026-03-19 23:19:08 -0300

    chore: add 014-random

---
+++ EstruturasRepeticao/014-random/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("## Random ##");
+
+Random random = new();
+
+//Console.WriteLine(random.Next());
+//Console.WriteLine(random.Next(20));
+//Console.WriteLine(random.Next(10, 20));
+//Console.WriteLine(random.NextDouble());
+
+int[] numerosSorteados = new int[6];
+
+for (int i = 0; i < numerosSorteados.Length; i++)
+{
+    int numero;
+    do
+    {
+        numero = random.Next(1, 61);
+    } while (numerosSorteados.Contains(numero));
+    numerosSorteados[i] = numero;
+}
+
+Array.Sort(numerosSorteados);
+Console.WriteLine("Números Sorteados");
+Console.WriteLine(string.Join(" ", numerosSorteados));
+
+
+Console.ReadKey();
