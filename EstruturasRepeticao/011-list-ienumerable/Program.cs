245d7949e80f10da73c4320065875dce22709ad8
GabrielChagas <gabrielserq@outlook.com>
2026-03-19 23:18:30 -0300

    chore: add 011-list-ienumerable

---
+++ EstruturasRepeticao/011-list-ienumerable/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("## List and IEnumerable ##");
+
+var numeros = new List<int> { 1, 2, 3, 4, 5 };
+
+var maiores = numeros.Where(x => x > 3).ToList();
+
+numeros.Add(10);
+
+foreach (var n in maiores)
+{
+    Console.WriteLine(n);
+}
+
+Console.ReadKey();
