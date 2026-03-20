b96f2d7d504fa2b1b129d18df62985610d72b108
GabrielChagas <gabrielserq@outlook.com>
2026-03-19 23:19:00 -0300

    chore: add 013-indexadores

---
+++ EstruturasRepeticao/013-indexadores/Program.cs
@@ -0,0 +1,25 @@
+﻿using _013_indexadores;
+
+Console.WriteLine("## Indexadores ##");
+
+Time time = new Time();
+
+time[0] = "São Paulo";
+time[1] = "Palmeiras";
+time[2] = "Santos";
+time[3] = "Corinthians";
+
+
+string valor = time[0];
+string valor2 = time[1];
+string valor3 = time[2];
+string valor4 = time[3];
+
+Console.WriteLine(valor);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+
+Console.ReadKey();
+
+
---
+++ EstruturasRepeticao/013-indexadores/Time.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_indexadores
+{
+    public class Time
+    {
+        //definir os limites do array
+        string[] valor = new string[10];
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= valor.Length)
+                {
+                    throw new IndexOutOfRangeException("Índice fora dos limites do array.");
+                }
+                return valor[index];
+            }
+            set
+            {
+                if (index < 0 || index >= valor.Length)
+                {
+                    throw new IndexOutOfRangeException("Índice fora dos limites do array.");
+                }
+                valor[index] = value;
+            }
+        }
+    }
+}
