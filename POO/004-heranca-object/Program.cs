20bdc78bc92ea9e02cb8d74b3bd9ac17a2fa350b
GabrielChagas <gabrielserq@outlook.com>
2026-03-21 15:02:52 -0300

    chore: add 003-heranca-object

---
+++ POO/004-heranca-object/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("## Herança Object ##");
+
+
+
+Console.ReadKey();
+
+public class ClassA
+{
+    
+}
+
+public class  ClassB
+{
+    
+}
+
+public struct StructA
+{
+
+}
+
+// Em C#, uma classe pode herdar de outra classe, mas não pode herdar de um struct.
+// No entanto, uma classe pode implementar interfaces, e um struct pode implementar interfaces também.
+//public class ClassDerivada : StructA
+//{
+//}
+
+// Em C#, uma classe só pode herdar de uma única classe base, mas pode implementar múltiplas interfaces.
+//public class ClassDerivada : ClassA, ClassB
+//{
+
+//}
