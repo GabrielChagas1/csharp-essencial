650c37981e95ed99aebda21768563ea2db2a1ffd
GabrielChagas <gabrielserq@outlook.com>
2026-03-23 23:53:20 -0300

    chore: add 006-heranca-override

---
+++ POO/006-heranca-override/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("## Herança Override ##");
+
+Gato gato = new() { Nome = "Mia" };
+gato.ExibeNome();
+
+Cachorro cachorro = new() { Nome = "Rex" };
+cachorro.ExibeNome();
+
+Console.ReadKey();
+
+class Animal
+{
+    public string Nome { get; set; }
+    public virtual void ExibeNome()
+    {
+        Console.WriteLine($"\nMeu nome é {Nome}");
+    }
+}
+
+class Gato : Animal
+{
+    //usando override para modificar o comportamento do método ExibeNome da classe base
+    public override void ExibeNome()
+    {
+        Console.WriteLine($"\nMeu nome é {Nome} e sou um gato.");
+    }
+}
+
+class Cachorro: Animal
+{
+
+}
