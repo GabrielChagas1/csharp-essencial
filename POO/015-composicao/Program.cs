de1fd27a34eccc3866d5499362487f7c22f42328
GabrielChagas <gabrielserq@outlook.com>
2026-03-25 12:58:36 -0300

    chore: add 015-composicao

---
+++ POO/015-composicao/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Composição");
+
+Console.ReadKey();
+
+public class Telhado { }
+public class Alicerce { }
+
+public class Casa
+{
+    private readonly Telhado _telhado;
+    private readonly Alicerce _alicerce;
+
+    public Casa()
+    {
+        _telhado = new Telhado();
+        _alicerce = new Alicerce();
+    }
+}
