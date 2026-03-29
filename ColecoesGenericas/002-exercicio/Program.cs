c1d6b9bc88c9280366085629c6282b87196e496b
GabrielChagas <gabrielserq@outlook.com>
2026-03-28 23:16:43 -0300

    chore: add 002-exercicios

--- ColecoesGenericas/002-exercicio/Program.cs
+++ ColecoesGenericas/002-exercicio/Program.cs
@@ -1,8 +1,44 @@
 ﻿Console.WriteLine("Exercício");
 
+ClasseGenerica<int> objInt = new();
+objInt.Adicionar(10);
+objInt.Adicionar(20);
+objInt.Adicionar(30);
+objInt.Adicionar(40);
+objInt.Adicionar(50);
+
+ClasseGenerica<string> objString = new();
+objString.Adicionar("Gabriel");
+
+
+Console.WriteLine($"Classe Generica<T> de objetos int\n");
+for (int i = 0; i < objInt.Length; i++)
+{
+    Console.WriteLine(objInt[i]);
+}
+
 Console.ReadKey();
 
 public class ClasseGenerica<T>
 {
     T[] obj = new T[5];
+    int contador = 0;
+
+    public int Length { get { return obj.Length; } private set { } }
+
+    public void Adicionar(T item)
+    {
+        if(contador <  obj.Length)
+        {
+            obj[contador] = item;
+        }
+        contador++;
+    }
+
+
+    public T this[int index]
+    {
+        get { return obj[index]; }
+        set { obj[index] = value; }
+    }
 }
