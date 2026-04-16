7c71046d443e97628d8efb823f4c74feb5612896
GabrielChagas <gabrielserq@outlook.com>
2026-04-15 22:47:27 -0300

    chore: add 007-colecoes-set

---
+++ ColecoesGenericas/007-colecoes-set/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Set");
+
+HashSet<int> numeros = [1, 2, 3, 4, 5];
+
+HashSet<string> frutas = ["uva", "banana", "pera"];
+
+HashSet<int> pares = [];
+
+for (int i = 0; i < 6; i++) pares.Add(i * 2);
+
+List<int> list = [1, 2, 3, 4, 5, 6, 7];
+HashSet<int> conjunto = new(list);
+
+Console.ReadKey();
