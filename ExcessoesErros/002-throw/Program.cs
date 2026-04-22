d8829f7c24313e6e65bf825b20be52c951fc26ed
GabrielChagas <gabrielserq@outlook.com>
2026-04-22 00:41:46 -0300

    chore: add 002-throw

---
+++ ExcessoesErros/002-throw/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Throw");
+
+try
+{
+    A.ProcessarA();
+}
+catch (Exception ex)
+{
+
+    ExceptionLogger.Log(ex, ex.Message);
+}
+
+Console.ReadKey();
+
+class A
+{
+    public static void ProcessarA() 
+    {
+        try
+        {
+            B.ProcessarB();
+        }
+        catch (Exception ex)
+        {
+            throw;
+            //ExceptionLogger.Log(ex, ex.Message);
+        }
+    }
+}
+
+class B
+{
+    public static void ProcessarB()
+    {
+        try
+        {
+            C.ProcessarC();
+        }
+        catch (Exception ex)
+        {
+            throw;
+            //ExceptionLogger.Log(ex, ex.Message);
+        }
+    }
+}
+
+
+class C
+{
+    public static void ProcessarC()
+    {
+       throw new NotImplementedException("Método não implementado");
+    }
+}
+
