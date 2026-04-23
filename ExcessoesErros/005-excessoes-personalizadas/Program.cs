af7b8b16d04a1354398e282c953549355d73e3c4
GabrielChagas <gabrielserq@outlook.com>
2026-04-23 15:16:22 -0300

    chore: add 005-excessoes-personalizadas

---
+++ ExcessoesErros/005-excessoes-personalizadas/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+Console.WriteLine("## Excessoes Personalizadas ##");
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
+try
+{
+    Conta conta1 = new(100, "Maria", 100m);
+    Console.WriteLine(conta1.ToString());
+    conta1.Depositar(100);
+    Console.WriteLine($"Saldo: {conta1.Saldo:C}");
+    conta1.Sacar(300);
+    Console.WriteLine($"Saldo: {conta1.Saldo:C}");
+}
+catch (Exception ex)
+{
+
+    ExceptionLogger.Log(ex, ex.Message);
+}
+Console.ReadKey();
+
+
+public class Conta(int numero, string? titular, decimal saldo)
+{
+    public int Numero {  get; set; } = numero;
+    public string? Titular { get; set; } = titular;
+    public decimal Saldo { get; set; } = saldo;
+
+    public decimal Depositar(decimal valor)
+    {
+        Saldo += valor;
+        Console.WriteLine($"Depositou: {valor:C}");
+        return Saldo;
+    }
+
+    public decimal Sacar(decimal valor)
+    {
+        if (Saldo < valor) throw new SaldoInsuficienteException(valor, Saldo);
+
+        Console.WriteLine($"Sacou: {valor:C}");
+        Saldo -= valor;
+        return Saldo;
+    }
+
+    public override string ToString()
+    {
+        return $"Conta: {Numero} : {Titular} - Saldo: {Saldo:C}";
+    }
+}
---
+++ ExcessoesErros/005-excessoes-personalizadas/SaldoInsuficienteException.cs
@@ -0,0 +1,11 @@
+﻿class SaldoInsuficienteException : Exception
+{
+    public SaldoInsuficienteException() { }
+
+    public SaldoInsuficienteException(string message) : base(message) { }
+
+    public SaldoInsuficienteException(string message, Exception inner) : base(message, inner) { }
+
+    public SaldoInsuficienteException(decimal saque, decimal saldo) : base($"Exception: Valor do saque {saque} é superior ao saldo {saldo}") { }
+
+}
