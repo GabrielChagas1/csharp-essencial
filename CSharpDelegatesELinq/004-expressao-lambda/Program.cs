4c477e1586638350a79b03a65971720950f55212
GabrielChagas <gabrielserq@outlook.com>
2026-04-24 11:28:23 -0300

    chore: add 004-expressao-lambda

---
+++ CSharpDelegatesELinq/004-expressao-lambda/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Expressoões Lambdas");
+
+List<string> nomes = new List<string> { "Maria", "Miriam", "Paulo", "Carlos" };
+
+string? resultado = nomes.Find(nome => nome.Equals("Paulo"));
+
+Console.WriteLine(resultado);
+
+Console.ReadKey();
+
