a5b2e234178253e0fde64e4df47039eb03817a9c
GabrielChagas <gabrielserq@outlook.com>
2026-04-24 11:27:40 -0300

    chore: add 001-multicasat-delegate

---
+++ CSharpDelegatesELinq/002-multicast-delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Multi Delegates");
+
+Delegado? delegado = new(Metodo1);
+delegado += Metodo2;
+delegado += Metodo3;
+
+delegado("Olá, multicast delegate!");
+delegado -= Metodo3;
+delegado("Olá, multicast delegate após remover o Método 3!");
+
+Console.ReadKey();
+
+static void Metodo1(string mensagem)
+{
+    Console.WriteLine($"Metodo 1: {mensagem}");
+}
+
+static void Metodo2(string mensagem)
+{
+    Console.WriteLine($"Metodo 2: {mensagem}");
+}
+
+static void Metodo3(string mensagem)
+{
+    Console.WriteLine($"Metodo 3: {mensagem}");
+}
+
+public delegate void Delegado(string mensagem);
