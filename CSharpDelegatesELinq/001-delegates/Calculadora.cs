743c58626859bd63dde72ef3c89492cecc24e973
GabrielChagas <gabrielserq@outlook.com>
2026-04-24 11:27:30 -0300

    chore: add 001-delegates

---
+++ CSharpDelegatesELinq/001-delegates/Calculadora.cs
@@ -0,0 +1,29 @@
+﻿namespace _001_delegates
+{
+    public class Calculadora
+    {
+        public static float Somar(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Subtrair(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float Multiplicar(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Dividir(float a, float b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Não é possível dividir por zero.");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ CSharpDelegatesELinq/001-delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using _001_delegates;
+
+Console.WriteLine("## Delegates ##");
+
+var resultado = Calculadora.Somar(10, 50);
+Console.WriteLine(resultado);
+
+OperacaoMatematica calc = new(Calculadora.Somar);
+var resultado1 = calc.Invoke(20, 30);
+Console.WriteLine(resultado1);
+
+OperacaoMatematica calc2 = Calculadora.Somar;
+var resultado2 = calc.Invoke(20, 30);
+Console.WriteLine(resultado2);
+
+OperacaoMatematica cal3 = (float a, float b) => Calculadora.Somar(a, b);
+var resultado3 = cal3(20, 30);
+Console.WriteLine(resultado3);
+
+calc = Calculadora.Multiplicar;
+var resultado4 = calc(20, 30);
+Console.WriteLine($"Multiplicar = {resultado4}");
+
+Console.ReadKey();
+
+public delegate float OperacaoMatematica(float a, float b);
