b97fb274ca9202115b5728c2235ff88e30a0eb91
GabrielChagas <gabrielserq@outlook.com>
2026-04-24 11:28:08 -0300

    chore: add 004-delegates-pre-definidos

---
+++ CSharpDelegatesELinq/004-delegate-pre-definidos/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Delegate Pré Definidos");
+
+//predicate é um delegate que recebe um parâmetro e retorna um booleano
+
+//action é um delegate que recebe um ou mais parâmetros e não retorna nada
+
+//func é um delegate que recebe um ou mais parâmetros e retorna um valor
+
+Console.Write("Informe um número inteiro: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+//if(VerificaPar(numero)) Console.WriteLine("O número {numero} é PAR");
+//else Console.WriteLine("O número {numero} é IMPAR");
+
+Console.WriteLine($"O número {numero} é {(numero % 2 == 0 ? "PAR" : "IMPAR")}");
+
+Console.ReadKey();
+
+static bool VerificaPar(int numero)
+{
+    return numero % 2 == 0;
+}
