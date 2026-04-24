f2d693e0dd3d2bfd90062e031e72f8831e34681d
GabrielChagas <gabrielserq@outlook.com>
2026-04-24 11:27:56 -0300

    chore: add 003-funcoes-anonimas

---
+++ CSharpDelegatesELinq/003-funcoes-anonimas/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Funções Anônimas");
+
+Imprimir imprimir = delegate (int valor)
+{
+    Console.WriteLine($"Valor: {valor}");
+};
+
+imprimir(100);
+
+List<string> nomes =  new List<string> { "Maria", "Miriam", "Paulo", "Carlos" };
+
+string? resultado = nomes.Find(VerificaNomeNaLista);
+
+string? resultado2 = nomes.Find(delegate (string nome)
+{
+    return nome.Equals("Paulo");
+});
+
+Console.WriteLine(resultado);
+Console.WriteLine(resultado2);
+
+Console.ReadKey();
+
+static bool VerificaNomeNaLista(string nome)
+{
+    return nome.Equals("Paulo");
+}
+
+public delegate void Imprimir(int valor);
