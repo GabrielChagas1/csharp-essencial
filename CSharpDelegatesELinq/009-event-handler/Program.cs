04e1936e72d2ccd754882a062956b2347837c2b2
GabrielChagas <gabrielserq@outlook.com>
2026-04-28 14:25:22 -0300

    chore: add 009-event-handler

---
+++ CSharpDelegatesELinq/009-event-handler/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("## EventHandler ##");
+
+Pedido pedido = new Pedido();
+pedido.NotificarPedidoCriado += SMS.EnviarSMS;
+pedido.NotificarPedidoCriado += Email.EnviarEmail;
+
+pedido.CriarPedido();
+
+Console.ReadKey();
+
+class Pedido
+{
+    public event EventHandler? NotificarPedidoCriado;
+    public void CriarPedido()
+    {
+        Console.WriteLine($"Pedido Criado");
+        if (NotificarPedidoCriado != null)
+        {
+            NotificarPedidoCriado(this, EventArgs.Empty);
+        }
+    }
+}
+
+class SMS
+{
+    public static void EnviarSMS(object? sender, EventArgs e)
+    {
+        Console.WriteLine($"Enviando SMS");
+    }
+}
+
+class Email
+{
+    public static void EnviarEmail(object? sender, EventArgs e)
+    {
+        Console.WriteLine($"Enviando Email");
+    }
+}
+
