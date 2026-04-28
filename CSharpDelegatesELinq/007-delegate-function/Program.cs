4b80bc1fa2be699fc94dad07dc15ff80f063933d
GabrielChagas <gabrielserq@outlook.com>
2026-04-28 14:25:05 -0300

    chore: add 007-delegate-function

---
+++ CSharpDelegatesELinq/007-delegate-function/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Delegate Func");
+
+Console.Write("Informe um número: ");
+double numero = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"\nA raiz quadrada de {numero} é: {CalculaRaiz(numero)}");
+
+Func<double, double> raizQuadrada = x => Math.Sqrt(x);
+Console.WriteLine($"\nA raiz quadrada de {numero} é: {raizQuadrada(numero)}");
+
+
+Console.ReadKey();
+
+static double CalculaRaiz(double num)
+{
+    return Math.Sqrt(num);
+}
