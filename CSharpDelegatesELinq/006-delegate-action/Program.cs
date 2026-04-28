2ef459e4c859b86b19adf006b31a1ce902b6492c
GabrielChagas <gabrielserq@outlook.com>
2026-04-28 14:24:50 -0300

    chore: add 006-delegate-action

---
+++ CSharpDelegatesELinq/006-delegate-action/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Delegate Action");
+
+Console.Write("Informe um número inteiro: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+Action<int> duplicar = x => Console.Write(x * 2);
+
+duplicar(numero);
+
+Console.ReadKey();
+
+static void Duplicar(int num)
+{
+    int resultado = num * 2;
+    Console.WriteLine(resultado);
+}
