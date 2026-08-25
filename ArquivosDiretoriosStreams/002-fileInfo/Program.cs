fc42f2b853a169aaff7d36c5063c9a1542f485d1
GabrielChagas <gabrielserq@outlook.com>
2026-08-24 22:38:40 -0300

    chore: add 002-fileInfo

---
+++ ArquivosDiretoriosStreams/002-fileInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("## FileInfo ##");
+
+string caminhoOrigem = @"C:\Users\GabrielChagas\Desktop\poesia\poesia.txt";
+string caminhoDestino = @"C:\Users\GabrielChagas\Desktop\poesia\txt\poesia.txt";
+string caminhoCopia = @"C:\Users\GabrielChagas\Desktop\poesia\poesia-copia.txt";
+
+FileInfo arquivo = new(caminhoOrigem);
+
+Console.WriteLine($"\nNome do arquivo: {arquivo.Name}");
+Console.WriteLine($"\nCaminho completo do arquivo: {arquivo.FullName}");
+Console.WriteLine($"\nO arquivo é somente leitura: {arquivo.IsReadOnly}");
+
+DirectoryInfo diretorioPai = arquivo.Directory;
+Console.WriteLine($"\nNome do diretorio: {diretorioPai.Name}");
+Console.WriteLine($"\nTamanho do arquivo: {arquivo.Length} bytes");
+Console.WriteLine($"Última gravação: {arquivo.LastWriteTime}");
+
+if (arquivo.Exists)
+{
+    Console.WriteLine($"\nO {caminhoOrigem} arquivo existe. Copiando para {caminhoCopia}");
+    arquivo.CopyTo(caminhoCopia);
+}
+else
+{
+    Console.WriteLine($"O {caminhoOrigem} não existe");
+}
+
+Console.WriteLine($"\nMovendo {caminhoOrigem} para {caminhoDestino}");
+arquivo.MoveTo(caminhoDestino);
+
+Console.ReadKey();
