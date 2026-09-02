3f40dd6834dc9342901036783d259b4338d4ec78
GabrielChagas <gabrielserq@outlook.com>
2026-09-02 12:29:39 -0300

    chore: add 009-eventos

---
+++ CSharpDelegatesELinq/009-eventos/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("## Eventos ##");
+
+Pedido pedido = new Pedido();
+pedido.NotificarPedidoCriado += SMS.EnviarSMS;
+pedido.NotificarPedidoCriado += Email.EnviarEmail;
+
+pedido.CriarPedido();
+
+Console.ReadKey();
+
+delegate void EnviarNotificacao();
+class Pedido
+{
+    public event EnviarNotificacao? NotificarPedidoCriado;
+    public void CriarPedido()
+    {
+        Console.WriteLine($"Pedido Criado");
+        if(NotificarPedidoCriado != null)
+        {
+            NotificarPedidoCriado();
+        }
+
+    }
+}
+
+class SMS
+{
+    public static void EnviarSMS()
+    {
+        Console.WriteLine($"Enviando SMS");
+    }
+}
+
+class Email
+{
+    public static void EnviarEmail()
+    {
+        Console.WriteLine($"Enviando Email");
+    }
+}
