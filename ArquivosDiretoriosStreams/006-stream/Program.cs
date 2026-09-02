94e95ff653406b4fbc2a7db4a3890acb4adcb502
GabrielChagas <gabrielserq@outlook.com>
2026-09-02 12:29:03 -0300

    chore: add 006-stream

---
+++ ArquivosDiretoriosStreams/006-stream/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("## Stream ##");
+
+var caminhoDiretorio = @"C:\Users\GabrielChagas\Desktop\poesia\txt";
+
+//FileStream fs = null;
+//StreamReader sr = null;
+
+//try
+//{
+//    fs = new FileStream(caminhoDiretorio + @"\poesia.txt", FileMode.Open, FileAccess.Read);
+//    sr = new StreamReader(fs);
+
+//    string? linha;
+//    while ((linha = sr.ReadLine()) != null)
+//    {
+//        Console.WriteLine(linha);
+//    }
+
+//}
+//catch(IOException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//finally
+//{
+//    sr?.Close();
+//    fs?.Close();
+//}
+
+try
+{
+    using FileStream fs = new(caminhoDiretorio + @"\poesia.txt", FileMode.Open, FileAccess.Read);
+    using StreamReader sr = new(fs);
+
+    string? linha;
+    while ((linha = sr.ReadLine()) != null)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadKey();
