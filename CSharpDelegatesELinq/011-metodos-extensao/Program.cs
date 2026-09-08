ea1347475be6e1b04f302a340bf3c839375f77d9
GabrielChagas <gabrielserq@outlook.com>
2026-09-08 10:46:16 -0300

    chore: add 011-metodos-extensao

---
+++ CSharpDelegatesELinq/011-metodos-extensao/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("## Extensão de métodos ##");
+
+string nome = "maysa";
+string nomeInvertido = nome.Inverter();
+Console.WriteLine(nomeInvertido);
+
+Console.ReadKey();
---
+++ CSharpDelegatesELinq/011-metodos-extensao/StringExtensionsBase.cs
@@ -0,0 +1,9 @@
+﻿public static class StringExtensions
+{
+    public static string Inverter(this string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
