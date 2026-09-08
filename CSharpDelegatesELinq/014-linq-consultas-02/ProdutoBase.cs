5e5d7080a09430b99570be1f49527583ae813893
GabrielChagas <gabrielserq@outlook.com>
2026-09-08 10:46:53 -0300

    chore: add 014-linq-consultas-02

---
+++ CSharpDelegatesELinq/014-linq-consultas-02/ProdutoBase.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public float Preco { get; set; }
+    public int Estoque { get; set; }
+    public string? Categoria { get; set; }
+
+
+    public static List<Produto> GetProdutos()
+    {
+        List<Produto> produtos = new List<Produto>()
+            {
+                new Produto {Id = 1, Nome= "Camiseta", Preco=29.90f, Estoque =3, Categoria = "Roupas"},
+                new Produto {Id = 2, Nome= "Meia Soc.", Preco=10.90f, Estoque =100, Categoria = "Roupas"},
+                new Produto {Id = 3, Nome= "Calça Jeans", Preco=90.00f, Estoque =3, Categoria = "Calçados"},
+                new Produto {Id = 4, Nome= "Tenis Nike", Preco=230.00f, Estoque =40, Categoria = "Tenis"},
+                new Produto {Id = 5, Nome= "Notebook", Preco=3000.90f, Estoque =4, Categoria = "Eletronicos"},
+                new Produto {Id = 6, Nome= "Mochila", Preco=29.90f, Estoque =30, Categoria = "Acessorios"},
+                new Produto {Id = 7, Nome= "Smart TV", Preco=1500.90f, Estoque =366, Categoria = "Eletronicos"},
+                new Produto {Id = 8, Nome= "Cadeira", Preco=40.90f, Estoque =20, Categoria = "Moveis"},
+                new Produto {Id = 9, Nome= "Mesa Sala", Preco=100.00f, Estoque =10, Categoria = "Moveis"},
+                new Produto {Id = 10, Nome= "Alicate", Preco=10.00f, Estoque =10, Categoria = "Ferramentas"}
+            };
+
+        return produtos;
+    }
+}
---
+++ CSharpDelegatesELinq/014-linq-consultas-02/Program.cs
@@ -0,0 +1,80 @@
+﻿Console.WriteLine("## LINQ Consultas 02 ##");
+
+List<Produto> produtos = Produto.GetProdutos();
+
+Console.WriteLine("\n First - Localiza o primeiro elemento");
+var primeiroProduto = produtos.First();
+Console.WriteLine($"{primeiroProduto.Nome} \t{primeiroProduto.Preco:C2}");
+
+try
+{
+    var primeiroProdutoNome = produtos.First(p => p.Nome == "Cadeiras");
+    Console.WriteLine($"{primeiroProdutoNome.Nome} \t{primeiroProdutoNome.Preco:C2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro: {ex.Message}");
+}
+
+var primeiroProdutoNome2 = produtos.FirstOrDefault(p => p.Nome == "Cadeiras");
+if(primeiroProdutoNome2 != null)
+{
+    Console.WriteLine($"{primeiroProdutoNome2.Nome} \t{primeiroProdutoNome2.Preco:C2}");
+}
+else
+{
+    Console.WriteLine("Produto não encontrado");
+}
+
+try
+{
+    var ultimoProduto = produtos.Last();
+    Console.WriteLine($"{ultimoProduto.Nome} \t{ultimoProduto.Preco:C2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro: {ex.Message}");
+}
+
+var ultimoProduto2 = produtos.LastOrDefault();
+if (ultimoProduto2 != null)
+{
+    Console.WriteLine($"{ultimoProduto2.Nome} \t{ultimoProduto2.Preco:C2}");
+}
+else
+{
+    Console.WriteLine("Produto não encontrado");
+}
+
+try
+{
+    var unicoProduto = produtos.Single();
+    Console.WriteLine($"{unicoProduto.Nome} \t{unicoProduto.Preco:C2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro: {ex.Message}");
+}
+
+try
+{
+    var unicoProduto2 = produtos.SingleOrDefault(x => x.Nome == "Cadeira");
+    if (unicoProduto2 != null)
+    {
+        Console.WriteLine($"{unicoProduto2.Nome} \t{unicoProduto2.Preco:C2}");
+    }
+    else
+    {
+        Console.WriteLine("Produto não encontrado");
+    }
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine($"Erro: {ex.Message}");
+}
+
+
+
+
+Console.ReadKey();
