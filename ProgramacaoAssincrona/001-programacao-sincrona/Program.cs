c1e39e9d125a1521c5869979be5110b8e312a87c
GabrielChagas <gabrielserq@outlook.com>
2026-09-10 17:11:32 -0300

    chore: add 001-programacao-sincrona

---
+++ ProgramacaoAssincrona/001-programacao-sincrona/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("## Programação Síncrona ##");
+
+int espera = 4000;
+
+Console.WriteLine("Tecle algo para iniciar");
+Console.ReadLine();
+
+RealizarTarefa(espera);
+
+Console.WriteLine($"\nTempo gasto {espera/1000} segundos");
+Console.WriteLine("\n>>>Fim do processamento");
+
+Console.ReadKey();
+
+static void RealizarTarefa(int tempo)
+{
+    Console.WriteLine("\n>>>Iniciando tarefa...");
+    Thread.Sleep(tempo);
+    Console.WriteLine(">>>Tarefa concluída!");
+}
