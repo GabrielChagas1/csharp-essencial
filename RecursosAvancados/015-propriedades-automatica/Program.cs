d30142edd331b21aaa90165e3d3cb820868d8586
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:14:34 -0300

    chore: add 015-propriedade-automatica

---
+++ RecursosAvancados/015-propriedades-automatica/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("## Propriedades Automáticas ##");
+
+Produto produto = new() { Preco = -10.5m };
+
+Console.WriteLine($"Preço do produto: {produto.Preco}");
+
+Console.ReadKey();
+
+public class Produto
+{
+    public decimal Preco {
+        get;
+        set => field = value < 0 ? throw new ArgumentException("O preço deve ser maior que zero.") : value;
+    }
+}
