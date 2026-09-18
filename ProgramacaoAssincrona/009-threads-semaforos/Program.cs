48d2af65c2c7c5b50bef462c42ed2b7c6fbf9e43
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:09:25 -0300

    chore: add 009-threads-assincronos

---
+++ ProgramacaoAssincrona/009-threads-semaforos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+
+Semaphore threadPool = new(3, 5);
+
+Console.WriteLine("Threads Semáfores");
+
+for (int i = 0; i < 10; i++)
+{
+    Thread threadObject = new(() => ProcessarOperacao(threadPool))
+    {
+        Name = $"Thread {i + 1}"
+    };
+    threadObject.Start();
+}
+
+Console.ReadLine();
+
+
+static void ProcessarOperacao(Semaphore threadPool)
+{
+    threadPool.WaitOne(); // Aguarda até que haja uma vaga no semáforo
+    Console.WriteLine($"{Thread.CurrentThread.Name} iniciando operação...");
+    Thread.Sleep(2000); // Simula uma operação demorada
+    threadPool.Release(); // Libera a vaga no semáforo para outra thread
+    Console.WriteLine($"{Thread.CurrentThread.Name} finalizando operação...");
+}
