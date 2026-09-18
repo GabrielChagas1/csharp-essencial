03fcae7537af3de49c9315538c5b0390a064ff0b
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:15:03 -0300

    chore: add 017-params-collection

---
+++ RecursosAvancados/017-params-collection/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("## Params e Collections ##");
+
+ContadorNumeros(1, 2, 3, 4, 5);
+
+
+Console.ReadKey();
+
+static void ContadorNumeros(params int[] numeros)
+{
+    Console.WriteLine($"Quantidade de números: {numeros.Length}");
+    //foreach (var numero in numeros)
+    //{
+    //    Console.WriteLine(numero);
+    //}
+}
