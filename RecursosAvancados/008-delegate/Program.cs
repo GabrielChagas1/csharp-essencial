cb3844294937c6886eff57caec588bf012a40452
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:12:08 -0300

    chore: add 008-delegates

---
+++ RecursosAvancados/008-delegate/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Hello, World!");
+
+ExibirMensagem acionador = new();
+
+Console.WriteLine("\nPressione algo para criar o delegate");
+Console.ReadKey();
+
+Notificador notificador = acionador.MostrarNaTela;
+notificador("Olá, mundo dos delegates!");
+Console.WriteLine("\nPressione algo para incluir outro método e chamar ambos");
+Console.ReadKey();
+
+notificador += acionador.EnviarEmail;
+notificador("Nova mensagem importante");
+Console.ReadKey();
+
+public delegate void Notificador(string mensagem);
+
+class ExibirMensagem
+{
+    public void MostrarNaTela(string mensagem)
+    {
+        Console.WriteLine($"Mensagem na tela: {mensagem}");
+    }
+    public void EnviarEmail(string mensagem)
+    {
+        Console.WriteLine($"Mensagem enviada por e-mail: {mensagem}");
+    }
+}
