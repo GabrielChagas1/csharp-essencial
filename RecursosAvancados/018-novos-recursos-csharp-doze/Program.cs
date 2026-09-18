63692cf599633ab65f6027195af4978db345da7c
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:15:17 -0300

    chore: add 018-novos-recursos

---
+++ RecursosAvancados/018-novos-recursos-csharp-doze/Program.cs
@@ -0,0 +1,38 @@
+﻿using ListaDeItens = System.Collections.Generic.List<string>;
+
+using Transacao = (decimal Valor, System.DateTime Data, TipoTransacao Tipo);
+List<Transacao> transacoes = new()
+{
+    (100.0m, DateTime.Now, TipoTransacao.Credito),
+    (50.0m, DateTime.Now, TipoTransacao.Debito),
+    (200.0m, DateTime.Now, TipoTransacao.Credito)
+};
+
+Console.WriteLine("Lista de Transações: \n");
+foreach (var transacao in transacoes)
+{
+    Console.WriteLine($"Valor: {transacao.Valor}, Data: {transacao.Data}, Tipo: {transacao.Tipo}");
+}
+
+//ListaDeItens lista = new()
+//{
+//    "item 1",
+//    "item 2",
+//    "item 3"
+//};
+
+//foreach (var item in lista)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+Console.ReadKey();
+
+
+public enum TipoTransacao
+{
+    Credito,
+    Debito
+}
