67d2d81447751355d634bff73eb6dccf9c4808aa
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:08:41 -0300

    chore: add 006-multiplas-excessoes

---
+++ ProgramacaoAssincrona/006-multiplas-excessoes/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("## Múltiplas Exceções ##");
+
+await LancaMultiplasExcessoesAsync();
+
+static async Task LancaMultiplasExcessoesAsync()
+{
+	Task tarefas = null!;
+    try
+	{
+		var primeiraTask = Task.Run(async () =>
+		{
+			await Task.Delay(2000);
+			throw new IndexOutOfRangeException("Index fora do intervalo.");
+		});
+
+		var segundaTask = Task.Run(async () =>
+		{
+			await Task.Delay(3000);
+			throw new InvalidOperationException("Operação inválida.");
+		});
+
+		tarefas = Task.WhenAll(primeiraTask, segundaTask);
+		await tarefas;
+    }
+	catch
+	{
+		if(tarefas?.Exception != null)
+		{
+            foreach (var ex in tarefas.Exception.InnerExceptions)
+            {
+                Console.WriteLine(ex.GetType().FullName);
+                Console.WriteLine("Exceção agregada capturada: " + ex.Message);
+            }
+        }
+        
+    }
+}
