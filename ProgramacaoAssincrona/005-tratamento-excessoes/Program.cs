c5b110f9ec2bff8f4d5db9857be632946f1d4d6a
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:08:28 -0300

    chore: add 005-tratamento-execoes

---
+++ ProgramacaoAssincrona/005-tratamento-excessoes/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("## Tratamento de Exceções ##");
+
+TesteAsync teste = new();
+await teste.ChamaTarefaAsync();
+
+
+class TesteAsync
+{
+    public static Task MinhaTarefaAsync()
+    {
+        return Task.Run(async () =>
+        {
+            await Task.Delay(2000);
+            throw new Exception("Ocorreu um erro na tarefa assíncrona.");
+        });
+    }
+
+    public async Task ChamaTarefaAsync()
+    {
+        try
+        {
+            await MinhaTarefaAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Este bloco não será executado");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
