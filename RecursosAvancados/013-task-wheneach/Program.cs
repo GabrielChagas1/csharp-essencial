ee276145c50d629bb0ef943d08e61faf347e5e0a
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:14:08 -0300

    chore: add 013-task-wheneach

---
+++ RecursosAvancados/013-task-wheneach/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("## Task.WhenEach ##");
+
+List<Task<int>> listaTarefas =
+[
+    SimulaOperacaoAsync(1000),
+    SimulaOperacaoAsync(2000),
+    SimulaOperacaoAsync(3000),
+    SimulaOperacaoAsync(5000)
+];
+
+//var tarefas = await Task.WhenAll(listaTarefas);
+
+
+await foreach (var resultado in Task.WhenEach(listaTarefas))
+{
+    Console.WriteLine($"Resultado: {await resultado}");
+}
+
+Console.ReadLine();
+
+Console.ReadKey();
+
+static async Task<int> SimulaOperacaoAsync(int delay)
+{
+    await Task.Delay(delay);
+    return delay;
+}
