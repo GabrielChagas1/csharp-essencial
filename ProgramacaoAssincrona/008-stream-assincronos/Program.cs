6400438c23d2c0e937f2909070e968427ec2e24a
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:09:11 -0300

    chore: add 008-stream-assincronos

---
+++ ProgramacaoAssincrona/008-stream-assincronos/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("streams assíncronos ");
+
+await foreach (var mes in GeraMeses())
+{
+    Console.WriteLine(mes);
+}
+
+Console.ReadLine();
+
+static async IAsyncEnumerable<string> GeraMeses()
+{
+    yield return "Janeiro";
+    yield return "Fevereiro";
+    yield return "Março";
+    yield return "Abril";
+    yield return "Maio";
+    yield return "Junho";
+    yield return "Julho";
+    yield return "Agosto";
+    await Task.Delay(1000); // Simula uma operação assíncrona
+    yield return "Setembro";
+    yield return "Outubro";
+    yield return "Novembro";
+    yield return "Dezembro";
+}
