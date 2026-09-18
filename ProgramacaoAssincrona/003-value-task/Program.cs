136a05c105d7f612f234517cf80065d405e2dba8
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:06:17 -0300

    chore: add 003-value-task

---
+++ ProgramacaoAssincrona/003-value-task/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## Value Task ##");
+
+Console.WriteLine("\nIniciando a operação assíncrona");
+await MetodoSemRetornoAsync();
+
+Console.WriteLine("\nIniciando a operação assíncrona");
+var resultado = await MetodoRetornaValorAsync(10);
+Console.WriteLine($"Resultado: {resultado}");
+
+Console.ReadKey();
+
+static async ValueTask MetodoSemRetornoAsync()
+{
+    Console.WriteLine("-Método sem retorno");
+    await Task.Delay(2000);
+}
+
+static async ValueTask<int> MetodoRetornaValorAsync(int valor)
+{
+    Console.WriteLine("-Método que retorna valor");
+    await Task.Delay(2000);
+    return valor * 2;
+}
