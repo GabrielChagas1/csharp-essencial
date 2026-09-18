906eb96dcac7564ea8115ca8f9dc9bf873bcdc6c
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:13:45 -0300

    chore: add 011-conditinal

---
+++ RecursosAvancados/011-csharp-14-conditional-assignment/Pedido.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _011_csharp_14_conditional_assignment
+{
+    public class Pedido
+    {
+        public Cliente? Cliente { get; set; }
+    }
+}
---
+++ RecursosAvancados/011-csharp-14-conditional-assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using _011_csharp_14_conditional_assignment;
+
+Console.WriteLine("## Conditional Assignment ##");
+
+string telefone = "11 99999-9999";
+
+//pedido null
+Pedido? pedido = null;
+
+pedido?.Cliente?.Telefone = telefone;
+Console.WriteLine("Código executado sem exceção");
+
+
+//pedido não null, mas cliente null
+Pedido pedido2 = new()
+{
+    Cliente = null
+};
+pedido2?.Cliente?.Telefone = telefone;
+Console.WriteLine("Cliente ainda é null.");
+
+//pedido e cliente não null
+Pedido pedido3 = new()
+{
+    Cliente = new Cliente()
+};
+
+pedido3?.Cliente?.Telefone = telefone;
+Console.WriteLine(pedido3!.Cliente.Telefone);
+
+Console.ReadKey();
