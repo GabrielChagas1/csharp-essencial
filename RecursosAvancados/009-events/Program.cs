2520fad8dcd86551466d631bbb98bc976da55cc0
GabrielChagas <gabrielserq@outlook.com>
2026-09-17 22:13:12 -0300

    chore: add 009-events

---
+++ RecursosAvancados/009-events/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("# Eventos #");
+
+Botao botao = new();
+
+botao.Clique += (sender, e) =>
+{
+    Console.WriteLine("Ouvinte 1");
+};
+
+botao.Clique += (sender, e) =>
+{
+    Console.WriteLine("Ouvinte 2");
+};
+
+botao.Clique += MostrarLog;
+
+Console.WriteLine("Pressione uma tecla para simular o clique no botão...");
+Console.ReadKey();
+botao.Clicar();
+Console.ReadKey();
+ 
+static void MostrarLog(object? sender, EventArgs e)
+{
+    Console.WriteLine("Log: Botão clicado!");
+}
+
+public class Botao
+{
+    public event EventHandler? Clique;
+    public void Clicar()
+    {
+        Console.WriteLine("Botão clicado!");
+        Clique?.Invoke(this, EventArgs.Empty);
+    }
+}
